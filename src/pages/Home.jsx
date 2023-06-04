d7d46b100f0e0c2304fcb479964c0a3983570cb3
davieduardo001 <ed.miranda@tutanota.com>
2023-06-04 18:21:55 -0300

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Container from '../components/Container'
+import savings from '../imgs/savings.svg'
+import styles from './Home.module.css'
+import LinkButton from '../components/LinkButton'
 
 const Home = () => {
   return (
     <Container customClass="min_height">
-        <h1>
-          HOME teste
-        </h1>
+      <section className={styles.home_container}>
+        <h1>Bem-vindo ao <span>Costs</span></h1>
+        <p>Comece a gerenciar seus projetos agora mesmo</p>
+
+        <LinkButton to="./newproject" text="Criar Projeto"/>
+        <img src={savings} alt="savings" />
+      </section>
+
     </Container>
   )
 }
